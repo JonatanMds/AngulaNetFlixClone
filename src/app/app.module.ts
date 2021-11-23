7be4c8a64f6ed605f3e6402d6d85e956f90da2e0
MoraesArmando <armando-am16@hotmail.com>
2021-11-23 02:47:14 -0300

    :sparkles:Component Modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    
   ],
   imports: [
     BrowserModule,
