50181ab786edeb57b8fa85961a29b1c8c035bce1
MoraesArmando <armando-am16@hotmail.com>
2021-11-24 20:38:21 -0300

    :bug:create methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ModalComponent } from './modal/modal.component';
-=======
 import { LoginFormComponent } from './login-form/login-form.component';
->>>>>>> aec6d38e8051dcc85047e773aec388c2ffc90ea4
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     ModalComponent,
-    
-=======
-    LoginFormComponent
->>>>>>> aec6d38e8051dcc85047e773aec388c2ffc90ea4
   ],
   imports: [
     BrowserModule,
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,13 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  @Input() stateModal = true
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  openModal(){
-
+  closeModalInfo(){
+    this.stateModal = false
   }
   
 
