424f217ced22c7f8ce1a24817d2c7b6b33e4be8b
Jonatan Marques <marques.jonatands@gmail.com>
2021-11-25 00:35:12 -0300

    responsividade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginFormComponent},
+  {path: 'main', component: MainPageComponent},
+  {path: 'home', component: HomeComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
