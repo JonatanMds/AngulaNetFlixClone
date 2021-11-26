a48223953538a3f01b969bdc707031113c4abffe
Israel Cena <israelsjm@gmail.com>
2021-11-26 17:23:32 -0300

    create guard for routes

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) { }
+  checkLogin() {
+    return localStorage.getItem('token');
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    this.authService.intendedUrl = state.url;
+    const shouldProceed =  Boolean(this.checkLogin())
+    if (shouldProceed) return shouldProceed;
+    return this.router.parseUrl('/login');
   }
   
 }
