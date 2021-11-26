2b1bca0278bce5ee1ed0e8bb3a53625b90940872
Jonatan Marques <marques.jonatands@gmail.com>
2021-11-26 17:50:58 -0300

    resposividade Main-Page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,6 +7,7 @@ import { GetUserService, UserInfo } from './get-user.service';
   styleUrls: ['./main-page.component.scss']
 })
 
+
 export class MainPageComponent implements OnInit{
 
   user: number = 1
@@ -19,3 +20,4 @@ export class MainPageComponent implements OnInit{
   }
 
 }
+
